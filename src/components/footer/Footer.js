401c5b392bc761ab6e4b711f4e10861d13279090
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-16 16:16:50 +0530

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Lightbox from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
-<<<<<<< HEAD
 import Bg from "../../images/footer.png";
 import pImg1 from "../../images/contact/contact-1.png";
 import pImg2 from "../../images/contact/contact-2.png";
@@ -12,17 +11,6 @@ import pImg4 from "../../images/contact/contact-4.png";
 import pImg5 from "../../images/contact/contact-5.png";
 import pImg6 from "../../images/contact/contact-6.png";
 import CtaSection from "../CtaSection/CtaSection";
-=======
-import Bg from '../../images/footer.png'
-import pImg1 from '../../images/contact/contact-1.png'
-import pImg2 from '../../images/contact/contact-2.png'
-import pImg3 from '../../images/contact/contact-3.png'
-import pImg4 from '../../images/contact/contact-4.png'
-import pImg5 from '../../images/contact/contact-5.png'
-import pImg6 from '../../images/contact/contact-6.png'
-import CtaSection from '../CtaSection/CtaSection';
-import { colors } from '@mui/material';
->>>>>>> 0890bc6fae1618dd40c7318f37b2efe2e0f02003
 
 const images = [pImg1, pImg2, pImg3, pImg4, pImg5, pImg6];
 
@@ -53,7 +41,6 @@ const Footer = (props) => {
                                     </div>
                                 ))}
                             </div> */}
-<<<<<<< HEAD
               <img
                 height={100}
                 width={150}
@@ -95,35 +82,6 @@ const Footer = (props) => {
           <div className="container">
             <div className="bottom-inner">
               {/* <div className="social-icons">
-=======
-                            <img height={100} width={150} src='https://www.telzonmarketing.in/images/main/telzonlogo.png'/>
-                            <p style={{ color: 'white' }}>
-                            At Telzon Marketing, we're masters at making your brand shine. We create content that tells your story in a way that everyone remembers. We're young, full of energy, and passionate about what we do.
-                            </p>
-                            <div className="social-icons">
-                                <Link onClick={ClickHandler} to="/"><i className="fa fa-twitter"></i></Link>
-                                <Link onClick={ClickHandler} to="/"><i className="fa fa-facebook"></i></Link>
-                                <Link onClick={ClickHandler} to="/"><i className="fa fa-instagram"></i></Link>
-                                <Link onClick={ClickHandler} to="/"><i className="fa fa-pinterest"></i></Link>
-                            </div>
-                        </div>
-                        <div className="col-xl-6 col-lg-4">
-                            <div className="contact-widget">
-                                <h3>Contact</h3>
-                                <p style={{ color: 'white' }}>+91 9307189776</p>
-                                <p style={{ color: 'white' }}>info@telzonmarketing.in</p>
-                                <p style={{ color: 'white' }}>Manish Nagar, Nagpur, Maharashtra 440037</p>
-                                <p style={{ color: 'white' }}>Kothrud, Pune 411038</p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer-bottom mt-55">
-                    <div className="container">
-                        <div className="bottom-inner">
-                            {/* <div className="social-icons">
->>>>>>> 0890bc6fae1618dd40c7318f37b2efe2e0f02003
                                 <Link onClick={ClickHandler} to="/"><i className="fa fa-twitter"></i></Link>
                                 <Link onClick={ClickHandler} to="/"><i className="fa fa-facebook"></i></Link>
                                 <Link onClick={ClickHandler} to="/"><i className="fa fa-instagram"></i></Link>
