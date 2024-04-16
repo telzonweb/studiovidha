bfa402fb35fbff6fcc56559f12527e5f9670c640
Mitali Desai <149577399+Mitudesai@users.noreply.github.com>
2024-04-16 12:26:06 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Js
+          Mitali Desai 
         </a>
       </header>
     </div>
