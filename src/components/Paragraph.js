ff2e13b40335e524ffae83d70582ec6d069ea8fa
Mitali Desai <149577399+Mitudesai@users.noreply.github.com>
2024-04-17 10:37:34 +0530

    updated about us section

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -2,16 +2,26 @@ import React from 'react'
 
 const Paragraph = () => {
   return (
-    <section className='pt-60 pl-60 pr-60 fs-5'>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    </p>
+    <section className='pt-60 pl-90 pr-80 fs-5'>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <img src="https://archello.s3.eu-central-1.amazonaws.com/images/2018/10/11/Contemporary-Modern-House-Design-6.1539270983.8601.jpg" alt="Your Image" className="img-fluid" />
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <h2>Our Goals</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, velit ac posuere
+            fermentum, arcu turpis tincidunt quam, sit amet scelerisque quam enim eget mi. In
+            tincidunt sapien nec felis congue, a commodo velit tempus. Duis vel tellus erat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, velit ac posuere
+            fermentum, arcu turpis tincidunt quam, sit amet scelerisque quam enim eget mi.
+          </p>
+        </div>
+      </div>
+    </div>
     </section>
   )
 }
