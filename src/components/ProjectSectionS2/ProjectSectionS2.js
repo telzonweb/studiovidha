d4715a8edd5b35c60138ca618c419def7c5afb99
Mitali Desai <149577399+Mitudesai@users.noreply.github.com>
2024-04-17 15:42:27 +0530

    portfolio updated

--- src/components/ProjectSectionS2/ProjectSectionS2.js
+++ src/components/ProjectSectionS2/ProjectSectionS2.js
@@ -1,43 +1,61 @@
-import React from 'react'
-import Projects from '../../api/Projects'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Projects from "../../api/Projects";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { BASEURL } from "../../Constant";
 
 const ProjectSectionS2 = (props) => {
+  const [portfolioData, setPortfolioData] = useState([]);
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    async function getPortfolio() {
+      const res = await fetch(`${BASEURL}/api/post/getposts?category=portfolio`);
+      const data = await res.json();
+      setPortfolioData(data.posts);
     }
-
-    return (
-        <section className="apartment-section s2 text-center">
-            <div className="container">
-                <div className="section-title mb-75">
-                    <h2>Choose an <span className="thin">Apartment</span></h2>
-                    <p>At vero eos et accusamus et iusto odio</p>
-                </div>
-                <div className="apartment-carousel">
-                    <div className="row">
-                    {Projects.slice(0, 6).map((project, i) => (
-                        <div className="col col-lg-4 col-md-6 col-12" key={i}>
-                            <div className="apartment-item">
-                                <div className="apartment-img">
-                                    <img src={project.pImg} alt="Apartment" />
-                                </div>
-                                <div className="apartment-content">
-                                    <img src={project.ssIcon} alt="Icon" />
-                                    <h3><Link onClick={ClickHandler} to={`/project-single/${project.slug}`}>{project.pTitle}</Link></h3>
-                                    <span>from {project.width} m2</span>
-                                    <p>{project.des} </p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+    getPortfolio();
+  }, []);
+console.log(portfolioData)
+  return (
+    <section className="apartment-section s2 text-center">
+      <div className="container">
+        <div className="section-title mb-75">
+          <h2>
+            Choose an <span className="thin">Apartment</span>
+          </h2>
+          <p>At vero eos et accusamus et iusto odio</p>
+        </div>
+        <div className="apartment-carousel">
+          <div className="row">
+            {/* First project */}
+            {
+                portfolioData.map((portfolio)=>(
+                    <div key={portfolio._id} className="col-lg-4 col-md-6">
+                        <Link to={portfolio.slug}>
+                        
+                    <div className="mb-4">
+                      <img
+                        src={portfolio.image}
+                        alt="Description of the image"
+                      />
+                      <h3>{portfolio.title}</h3>
                     </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+                    </Link>
+                  </div>
+                ))
+            }
+           
+           
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default ProjectSectionS2;
