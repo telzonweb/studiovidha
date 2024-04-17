ca5dfc9e6cb2567e11144da8221a9b7dda2ff024
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-17 13:55:58 +0530

    "Update BlogList component to use local state and pagination; remove Footer and Scrollbar from ProjectPage"
    This commit updates the BlogList component to use local state for managing the blog posts and implements pagination. The Footer

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import BlogSidebar from "../BlogSidebar";
 
 import blogs from "../../api/blogs";
+import { domain } from "../../domain";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [pageNo, setPageNo] = useState(0);
   const ClickHandler = () => {
     window.scrollTo(10, 0);
   };
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    async function getBlogs() {
+      // const res = await fetch("http://localhost:5173/api/post/getPosts");
+      let data;
+      if (pageNo !== 0) {
+        const res = await fetch(
+          `${domain}/api/post/getposts?startIndex=${pageNo * 6}?limit=6?`
+        );
+
+        data = await res.json();
+      } else {
+        const res = await fetch(`${domain}/api/post/getposts?limit=6`);
+
+        data = await res.json();
+      }
+      // console.log(data.posts);
+      setPosts(data.posts);
+    }
+    getBlogs();
+    // console.log(posts);
+  }, [pageNo]);
+
+  function handlePageIncrease() {
+    if (pageNo * 6 < posts.length) setPageNo(pageNo + 1);
+    // console.log(pageNo);
+  }
+  function handlePageReduce() {
+    // console.log(postData.length);
+    if (pageNo > 0) setPageNo(pageNo - 1);
+    // console.log(pageNo);
+  }
+
   return (
     <section className="blog-list mt-150 mb-50">
+      {/* {console.log(posts)} */}
       <div className="container">
         <div className="row text-center">
           <div className="col-lg-8 mx-auto">
-            {blogs.map((blog, bitem) => (
+            {posts.map((blog, bitem) => (
               <div className="blog-item" key={bitem}>
                 <div className="blog-image">
-                  <img src={blog.blogSingleImg} alt="Blog Image" />
+                  <img src={blog.image} alt="Blog Image" />
                   <span className="date">{blog.create_at}</span>
                 </div>
                 <div className="blog-content">
