911befc649e81226818d7ab9922b52da1024bf70
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-18 17:26:15 +0530

    Update index.html and ProjectSinglePage components
    The index.html file was updated to change the favicon, title, and description. The Header component was also updated to use a new logo image. The ProjectSinglePage component was updated to

--- src/main-component/ProjectSinglePage/ProjectSinglePage.js
+++ src/main-component/ProjectSinglePage/ProjectSinglePage.js
@@ -27,13 +27,18 @@ const ProjectSinglePage = (props) => {
   return (
     <Fragment>
       <Navbar />
-      <PageTitle pageTitle={portfolioDetails.title} pagesub={"Project"} />
+      {/* <PageTitle pageTitle={portfolioDetails.title} pagesub={"Project"} /> */}
+      <PageTitle pagesub={"Project"} />
       <div className="project-single-area section-padding">
+        <h3 className="blog-title">{portfolioDetails.title}</h3>
         <div className="property-single mt-150">
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
-                <img src={portfolioDetails.image} />
+                <img
+                  src={portfolioDetails.image}
+                  alt={portfolioDetails.title}
+                />
                 <div
                   className="p-3 max-w-2xl mx-auto w-full post-content"
                   dangerouslySetInnerHTML={{ __html: portfolioDetails.content }}
